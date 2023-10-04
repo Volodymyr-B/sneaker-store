b95b74f5f4f0a35bcaa8179b7ac94cf697e9104f
VolodymyrB <vladimir.babich87@gmail.com>
2023-10-04 10:52:01 +0300

    small styling improve

--- src/app/[...category]/components/product-card.tsx
+++ src/app/[...category]/components/product-card.tsx
@@ -12,21 +12,26 @@ export const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
     <>
       <Link prefetch={false} href={`/shop/${product.id}`}>
-        <article className="flex flex-col gap-1">
+        <article
+          className="lg:hover:outline lg:hover:outline-1 transition-all
+        outline-app-primary_action rounded-sm lg:hover:shadow-lg"
+        >
           <AppImage src={product.cover} alt={product.name} />
-          <h5>{product.name}</h5>
-          <div className="flex gap-2">
-            <p>{product.sport}</p>
-            <p>{product.type}</p>
+          <div className="flex flex-col gap-1 mt-1 pl-2">
+            <h5>{product.name}</h5>
+            <div className="flex gap-2">
+              <p>{product.sport}</p>
+              <p>{product.type}</p>
+            </div>
+            {product.isSale ? (
+              <DiscountedPrice
+                discount={product.discount}
+                price={product.price}
+              />
+            ) : (
+              <p>{product.price} $</p>
+            )}
           </div>
-          {product.isSale ? (
-            <DiscountedPrice
-              discount={product.discount}
-              price={product.price}
-            />
-          ) : (
-            <p>{product.price} $</p>
-          )}
         </article>
       </Link>
     </>
